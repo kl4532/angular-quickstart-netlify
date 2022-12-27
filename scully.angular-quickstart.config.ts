a62247e4cd3d1d54d21256254b2dc5667d787222
kl432 <>
2022-12-27 10:38:59 +0100

    Revert "scully WIP"
    This reverts commit 11472b02bccf6c484526d77bf2d51d7d7f914f3f.

--- scully.angular-quickstart.config.ts
+++ scully.angular-quickstart.config.ts
@@ -2,12 +2,13 @@ import { ScullyConfig } from '@scullyio/scully';
 
 /** this loads the default render plugin, remove when switching to something else. */
 import '@scullyio/scully-plugin-puppeteer'
+import {environment} from "./src/environments/environment";
 
 export const config: ScullyConfig = {
   projectRoot: "./src",
   projectName: "angular-quickstart",
   // add spsModulePath when using de Scully Platform Server,
-  puppeteerLaunchOptions: {executablePath: '/Applications/Chromium.app/Contents/MacOS/Chromium'},
+  puppeteerLaunchOptions: {executablePath: environment.CHROMIUM_PATH},
   outDir: './dist/static',
   routes: {
   }
