a13555bf3476cefae23541c17276a9ede5668d97
kl432 <>
2022-12-27 11:01:02 +0100

    wip scully

--- scully.angular-quickstart.dev.config.ts
+++ scully.angular-quickstart.dev.config.ts
@@ -1,8 +1,10 @@
 import { ScullyConfig } from '@scullyio/scully';
 
+import '@scullyio/scully-plugin-puppeteer'
 export const config: ScullyConfig = {
   projectRoot: "./src",
   projectName: "angular-quickstart",
+  puppeteerLaunchOptions: {executablePath: '/Applications/Chromium.app/Contents/MacOS/Chromium' || {}},
   outDir: './dist/static',
   routes: {
   }
