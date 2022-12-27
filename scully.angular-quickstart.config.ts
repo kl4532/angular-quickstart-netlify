b6afa603a1aac3fb627f8c326e2ad2c4d470dc50
kl432 <>
2022-12-27 10:49:39 +0100

    wip scully

--- scully.angular-quickstart.config.ts
+++ scully.angular-quickstart.config.ts
@@ -1,13 +1,10 @@
 import { ScullyConfig } from '@scullyio/scully';
 
 /** this loads the default render plugin, remove when switching to something else. */
-import '@scullyio/scully-plugin-puppeteer'
 
 export const config: ScullyConfig = {
   projectRoot: "./src",
   projectName: "angular-quickstart",
-  // add spsModulePath when using de Scully Platform Server,
-  puppeteerLaunchOptions: {executablePath: '/Applications/Chromium.app/Contents/MacOS/Chromium' || {}},
   outDir: './dist/static',
   routes: {
   }
