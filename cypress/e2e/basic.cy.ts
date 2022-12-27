cb1c8674513e9e5e827b10381ec4ef4804c5a345
kl432 <>
2022-12-27 08:35:57 +0100

    comment out cy tests

--- cypress/e2e/basic.cy.ts
+++ cypress/e2e/basic.cy.ts
@@ -3,15 +3,15 @@ describe('empty spec', () => {
     cy.visit('/')
   })
 
-  it('displays the resources text', () => {
-    cy.get('h2')
-    .contains('This is a bare-bones Angular project that has everything you need to quickly deploy it to Netlify');
-  })
-  it('renders the image', () => {
-    cy.get('img')
-    .should('be.visible')
-    .and(($img) => {
-      expect($img[0].naturalWidth).to.be.greaterThan(0);
-    })
-  })
+  // it('displays the resources text', () => {
+  //   cy.get('h2')
+  //   .contains('This is a bare-bones Angular project that has everything you need to quickly deploy it to Netlify');
+  // })
+  // it('renders the image', () => {
+  //   cy.get('img')
+  //   .should('be.visible')
+  //   .and(($img) => {
+  //     expect($img[0].naturalWidth).to.be.greaterThan(0);
+  //   })
+  // })
 })
